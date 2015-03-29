39071c74e0998aaf2ded4e32b335250461b96c64
Steve Swett <sswett@thinkful.com>
2015-03-29 19:16:42 -0400

    First pass at submitting an answer and advancing to next question

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 var questionsArray = new Array();
+var currentQuestion = 0;   // 0-based index
 var DEFAULT_ANSWER_NO = -1;
 
 
 $(document).ready(function() {
 
-	createAndAddQuestion("How old is Steve?", [40, 45, 50, 55, 51], 5, '');
-	createAndAddQuestion("How old is Peggy?", [35, 40, 45, 50, 55], 4, '');
-	createAndAddQuestion("How old is Tanner?", [21, 22, 23, 24, 25], 2, '');
-	createAndAddQuestion("How old is Andy?", [19, 20, 21, 22, 23], 2, '');
+	setupQuestions();
 
 	// logQuestions();   // looks good
 
@@ -15,15 +13,61 @@ $(document).ready(function() {
 		startOver();
 	});
 
+	$("#submit-answer-button").click(function() {
+		submitAnswer();
+	});
+
 	startOver();
 
 });
 
 
+function setupQuestions()
+{
+	createAndAddQuestion("How old is Steve?", [40, 45, 50, 55, 51], 5, '');
+	createAndAddQuestion("How old is Peggy?", [35, 40, 45, 50, 55], 4, '');
+	createAndAddQuestion("How old is Tanner?", [21, 22, 23, 24, 25], 2, '');
+	createAndAddQuestion("How old is Andy?", [19, 20, 21, 22, 23], 2, '');
+}
+
+
 function startOver()
 {
 	resetResponsesAndStats();
-	displayQuestion(0);
+	currentQuestion = 0;
+	displayCurrentQuestion();
+	hideSubmissionError();
+}
+
+
+function hideSubmissionError()
+{
+	$("#submission-error").hide();
+}
+
+
+function submitAnswer()
+{
+	hideSubmissionError();
+	var answer = parseInt( $("input:radio[name=answer]:checked").val() || DEFAULT_ANSWER_NO );
+
+	if (answer < 0 || answer > 4)
+	{
+		$("#submission-error").show();
+		return;
+	}
+
+	questionsArray[currentQuestion].selectedAnswerNo = answer;
+
+	if (currentQuestion < questionsArray.length - 1)
+	{
+		currentQuestion += 1;
+		displayCurrentQuestion();
+	}
+	else
+	{
+		logQuestions();   // temporary
+	}
 }
 
 
@@ -59,6 +103,7 @@ function logQuestions()
 
 		console.log("correctAnswerNo: " + question.correctAnswerNo);
 		console.log("imageFilename: " + question.imageFilename);
+		console.log("selectedAnswerNo: " + question.selectedAnswerNo);
 		console.log("");
 	}
 }
@@ -81,15 +126,19 @@ function resetResponsesAndStats()
 }
 
 
-function displayQuestion(pQuestionIndex)
+function displayCurrentQuestion()
 {
-	$("#question-no").html((pQuestionIndex + 1));
-	var question = questionsArray[pQuestionIndex];
+	$("#question-no").html((currentQuestion + 1));
+	var question = questionsArray[currentQuestion];
 	$("#question-text").html(question.questionText);
 
 	for (var a = 0; a < question.answerTextArray.length; a++)
 	{
 		$("#answer-" + a + "-text").html(question.answerTextArray[a]);
 	}
+
+	$("input:radio[name=answer]").prop("checked", false);
+
+	// TODO: If the answer is on file already, check it:
 
 }
